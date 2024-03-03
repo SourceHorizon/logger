6d3d90c17d4b515784222154751903882fc8c71c
Bungeefan <severin.hamader@yahoo.de>
2024-03-03 22:42:07 +0100

    Added web safe export, closes #58

---
+++ lib/web.dart
@@ -0,0 +1,22 @@
+/// Web-safe logger.
+library web;
+
+export 'src/ansi_color.dart';
+export 'src/filters/development_filter.dart';
+export 'src/filters/production_filter.dart';
+export 'src/log_event.dart';
+export 'src/log_filter.dart';
+export 'src/log_level.dart';
+export 'src/log_output.dart';
+export 'src/log_printer.dart';
+export 'src/logger.dart';
+export 'src/output_event.dart';
+export 'src/outputs/console_output.dart';
+export 'src/outputs/memory_output.dart';
+export 'src/outputs/multi_output.dart';
+export 'src/outputs/stream_output.dart';
+export 'src/printers/hybrid_printer.dart';
+export 'src/printers/logfmt_printer.dart';
+export 'src/printers/prefix_printer.dart';
+export 'src/printers/pretty_printer.dart';
+export 'src/printers/simple_printer.dart';
