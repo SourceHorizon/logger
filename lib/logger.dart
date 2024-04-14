4adf60c3aa8e15bf131c673c68d21ed62ad0f6fb
Bungeefan <severin.hamader@yahoo.de>
2024-04-15 00:45:36 +0200

    Removed obsolete web safe export

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,8 +1,27 @@
 /// Small, easy to use and extensible logger which prints beautiful logs.
 library logger;
 
-export 'src/outputs/file_output_stub.dart'
-    if (dart.library.io) 'src/outputs/file_output.dart';
+export 'src/ansi_color.dart';
+export 'src/date_time_format.dart';
+export 'src/filters/development_filter.dart';
+export 'src/filters/production_filter.dart';
+export 'src/log_event.dart';
+export 'src/log_filter.dart';
+export 'src/log_level.dart';
+export 'src/log_output.dart';
+export 'src/log_printer.dart';
+export 'src/logger.dart';
+export 'src/output_event.dart';
 export 'src/outputs/advanced_file_output_stub.dart'
     if (dart.library.io) 'src/outputs/advanced_file_output.dart';
-export 'web.dart';
+export 'src/outputs/console_output.dart';
+export 'src/outputs/file_output_stub.dart'
+    if (dart.library.io) 'src/outputs/file_output.dart';
+export 'src/outputs/memory_output.dart';
+export 'src/outputs/multi_output.dart';
+export 'src/outputs/stream_output.dart';
+export 'src/printers/hybrid_printer.dart';
+export 'src/printers/logfmt_printer.dart';
+export 'src/printers/prefix_printer.dart';
+export 'src/printers/pretty_printer.dart';
+export 'src/printers/simple_printer.dart';
